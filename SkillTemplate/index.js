b9a4d9f8605fa7f13ea615a9632e40e796e99a72
Keheira <keheirah@gmail.com>
2017-05-03 01:40:08 -0400

    Added excercises and start of plan build

--- SkillTemplate/index.js
+++ SkillTemplate/index.js
@@ -6,10 +6,24 @@ var SKILL_NAME = 'Exercise'; //replace
 var defaultWorkoutTime = 5;
 
 function buildWorkOut(minutes) {
-    
-    
-    
+    vart start = Math.floor(Date.now()/1000); //current time in secs
+    var finish = start + (minutes * 60);
+    while (Math.floor(Date.now()/1000) != finish){
+        //get excercise a lil lost
+    }
 }
+
+var excercise = [
+    "10 pushups",
+    "15 jumping jacks",
+    "10 crunches",
+    "10 dead bugs",
+    "15 swats",
+    "15 bycles",
+    "run in place for a minute",
+    "10 pullups"
+    "10 curls"
+];
 
 
 exports.handler = function(event, context, callback) {
@@ -25,7 +39,7 @@ var handlers = {
     },
     'GETPLAN': function () {
     var minutes = this.event.request.type == 'LaunchRequest' || this.event.request.intent.slots.minutes.value === undefined ? defaultWorkoutTime : parseInt(this.event.request.intent.slots.minutes.value);
-        //buildWorkOut();
+    buildWorkOut();
     this.emit(':tell', 'Here is a ' + minutes  + ' minute workout.');
     },
     'AMAZON.HelpIntent': function () {
