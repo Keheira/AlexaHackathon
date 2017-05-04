f62b80f23ec5f78bc5977eef6f5bd6e6e5d842fa
parisbutterfield <parisbdev@gmail.com>
2017-05-04 01:36:53 -0400

    Some basic logic for picking exercises.

--- SkillTemplate/index.js
+++ SkillTemplate/index.js
@@ -3,15 +3,77 @@ var Alexa = require('alexa-sdk');
 
 var APP_ID = ''; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 var SKILL_NAME = 'Exercise'; //replace
-var defaultWorkoutTime = 5;
 
+var defaultWorkoutTime = 5;
+var maxExcerciseTime = 150;
+
+
+
+/*
+Builds a workouut 
+
+Example:
+currentSpeech : Here are some excercises you can do 10 pushups 10 pushups 10 pushups 40 pushups 20 pushups 10 pushups 
+
+Todo: combine count result if like excercise. So 10 pushups 10 pushups. = 30 pushups
+
+
+*/
 function buildWorkOut(minutes) {
-    vart start = Math.floor(Date.now()/1000); //current time in secs
-    var finish = start + (minutes * 60);
-    while (Math.floor(Date.now()/1000) != finish){
-        //get excercise a lil lost
+   var secondsLeft = (minutes * 60);
+   var excerciseSpeech = "Here are some excercises you can do ";
+    while (secondsLeft > 0 ){
+        var currentExcerciseTime = getExcerciseForTime(secondsLeft);
+        var list = excerciseTimes[currentExcerciseTime].list; 
+        var excercise = list[getRandomInt(0,list.length-1)*1];
+        excerciseSpeech += excercise + " ";
+        secondsLeft = secondsLeft - currentExcerciseTime;
+
     }
+
+    console.log(excerciseSpeech);
 }
+
+/*
+Gets an excercise given the number of seconds. The excerciseTimes object has a maxTime of 150 seconds 2.5 seconds.
+Returns (30, 60, 90, 120, 150). Example after calling this method with 5 minutes in the loop above possible output
+could be:
+    60
+
+    30
+
+    120
+
+    60
+
+    30
+*/
+function getExcerciseForTime(seconds) {
+    var base = 30;
+    return Math.min((getRandomInt(1, seconds / base) * base), (getRandomInt(1, maxExcerciseTime / base) * base));
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+/*
+This object contains a list of excercises for a given number of seconds. Each group of seconds
+can have an array of excercises. You can then find a random in each group using getRandomInt(1. 30.length)
+
+Ex: 30: {["10 Pushups", "30 Situps"]} 
+
+
+*/
+var excerciseTimes = {
+    30: {list: ["10 pushups"]},
+    60: {list: ["20 pushups"]},
+    90: {list: ["30 pushups"]},
+    120: {list: ["40 pushups"]}, 
+    150: {list: ["50 pushups"]},
+};
 
 var excercise = [
     "10 pushups",
@@ -21,8 +83,8 @@ var excercise = [
     "15 swats",
     "15 bycles",
     "run in place for a minute",
-    "10 pullups"
-    "10 curls"
+    "10 pullups",
+    "10 curls",
 ];
 
 
@@ -39,7 +101,7 @@ var handlers = {
     },
     'GETPLAN': function () {
     var minutes = this.event.request.type == 'LaunchRequest' || this.event.request.intent.slots.minutes.value === undefined ? defaultWorkoutTime : parseInt(this.event.request.intent.slots.minutes.value);
-    buildWorkOut();
+    buildWorkOut(minutes);
     this.emit(':tell', 'Here is a ' + minutes  + ' minute workout.');
     },
     'AMAZON.HelpIntent': function () {
